6172dc5f512edcfa7889a1809fbe6f7e615b89c2
Gauri <dahakegauri4@gmail.com>
2024-10-07 00:09:43 +0530

    SreadOprater

--- SpreadOprater/src/App.jsx
+++ SpreadOprater/src/App.jsx
@@ -14,4 +14,21 @@ const App=()=>{
   const handleSubmit=()=>{
     console.log(input);
   }
+  return(
+    <>
+    <h1>Application from</h1>
+    Enter name : <input type="text" name="name" value={input.name} onChange={handleInput}/>
+    <br />
+    Enter city : <input type="text" name="city" value={input.city} onChange={handleInput}/>
+    <br />
+    Enter contact : <input type="text" name="contact" value={input.contact} onChange={handleInput}/>
+    <br />
+    Enter email : <input type="text" name="email" value={input.email} onChange={handleInput}/>
+    <br />
+
+    <button onClick={handleInput}>Data Save !</button>
+    </>
+  )
+  
 }
+export default App;
