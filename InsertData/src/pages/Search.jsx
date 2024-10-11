5bc8f6425978a43898ca28c6ff5f5b3418e789ac
Gauri <dahakegauri4@gmail.com>
2024-10-11 22:15:14 +0530

    saerching

--- InsertData/src/pages/Search.jsx
+++ InsertData/src/pages/Search.jsx
@@ -1,33 +1,36 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState } from "react"
 
-const SearchByName=()=>{
-    const [ename, setEname]=useState("");
-    const [mydata, setmydata]=useState([]);
-    const handleChange=(e)=>{
-        let empname = e.target.value;
-        setEname(empname);
+const Search=()=>{
+    const [eno, setEno]=useState("");
+    const [mydata, setMydata]=useState([]);
+    
+    const handleSubmit=()=>{
         let api=`http://localhost:3000/employees`;
         axios.get(api).than((res)=>{
-            setMydata(res.data);
-            console.log(res.data);
+        setMydata(res.data);
+        console.log(res.data);
         })
     }
+
     const ans=mydata.map((key)=>{
-        let str=key.name;
-        let mystatus=str.include(ename);
-        console.log(mystatus);
-        if(mystatus)
-        {
-            return(
-                <>
-                <tr>
-                    <td> { key.empno}</td>
-                    <td>{key.name}</td>
-                    <td>{key.email}</td>
-                    <td>{key.contact}</td>
-                </tr>
-                </>
-            )
-        }
+        return(
+            <>
+            <h1>Employee Number:{key.empno}</h1>
+            <h2> Name : {key.name}</h2>
+            <h2> Email : {key.email}</h2>
+            <h2> Contact no: {key.Contact}</h2>
+            </>
+        )
     })
+    return(
+        <>
+        <h1> Search Empplyee Records</h1>
+    Enter Emp No. : <input type="text" value={eno} onChange={(e)=>{setEno(e.target)}}/>
+    <button onClick={handleSubmit}>search</button>
+    <hr size="5" color="green"/>
+    {ans}
+        </>
+    )
 }
+export default Search;
---
+++ InsertData/src/pages/SearchByName.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const SearchByName=()=>{
+    const [ename, setEname]=useState("");
+    const [mydata, setmydata]=useState([]);
+    const handleChange=(e)=>{
+        let empname = e.target.value;
+        setEname(empname);
+        let api=`http://localhost:3000/employees`;
+        axios.get(api).than((res)=>{
+            setMydata(res.data);
+            console.log(res.data);
+        })
+    }
+    const ans=mydata.map((key)=>{
+        let str=key.name;
+        let mystatus=str.include(ename);
+        console.log(mystatus);
+        if(mystatus)
+        {
+            return(
+                <>
+                <tr>
+                    <td> { key.empno}</td>
+                    <td>{key.name}</td>
+                    <td>{key.email}</td>
+                    <td>{key.contact}</td>
+                </tr>
+                </>
+            )
+        }
+    })
+}
+export default SearchByName;
